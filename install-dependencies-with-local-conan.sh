ae70f1cc7659f2556e1caca86b8512a53abbb9e9
Falko Schwabe <falko.schwabe@gmail.com>
2020-01-29 20:57:57 +0100

    try to install conan silently when installing dependencies
    Required for cygwin environments when conan is available through the windows host system. Otherwise conan will generate Windows specific paths, e.g. "C:\foo\bar"

--- install-dependencies-with-local-conan.sh
+++ install-dependencies-with-local-conan.sh
@@ -7,6 +7,11 @@ if [ "${ARCH_TYPE}" = "x64" ]; then
     CONAN_ARCH_TYPE="x86_64"
 fi
 
+# Try having conan available in cygwin although conan is already installed for the parent Windows system
+command -v python3 &>/dev/null || { echo "Please make sure 'python3' is available."; exit 1; }
+python3 -m pip install conan &>/dev/null
+
+command -v conan &>/dev/null || { echo "Please make sure 'conan' is available."; exit 1; }
 conan install . \
     --install-folder "build/conan/${ARCH_TYPE}" \
     --build missing \
